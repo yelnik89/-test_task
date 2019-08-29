19b947e185bd3623ade55c02967aea4ab68d4f22
yelnik <edv@attrax.digital>
2019-08-29 11:00:57 +0700

    task-4 auth work, hard set user name and password

--- src/main/java/test_task/demo/config/WebSecurityConfig.java
+++ src/main/java/test_task/demo/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 @Configuration
@@ -21,6 +22,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 
     @Autowired
     private AuthenticationEntryPoint authEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        String password = "123";
+        String encrytedPassword = this.passwordEncoder().encode(password);
+        System.out.print("Encoded password of 123=" + encrytedPassword);
+
+        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> mngConfig = auth.inMemoryAuthentication();
+
+        UserDetails u1 = User.withUsername("tom").password(encrytedPassword).roles("USER").build();
+        UserDetails u2 = User.withUsername("jerry").password(encrytedPassword).roles("USER").build();
+
+        mngConfig.withUser(u1);
+        mngConfig.withUser(u2);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -33,28 +49,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     public BCryptPasswordEncoder passwordEncoder(){
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        String password = "123";
-        String encrytedPassword = this.passwordEncoder().encode(password);
-        System.out.print("Encoded password of 123=" + encrytedPassword);
-
-        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> mngConfig = auth.inMemoryAuthentication();
-
-        // Defines 2 users, stored in memory.
-        // ** Spring BOOT >= 2.x (Spring Security 5.x)
-        // Spring auto add ROLE_
-        UserDetails u1 = User.withUsername("tom").password(encrytedPassword).roles("USER").build();
-        UserDetails u2 = User.withUsername("jerry").password(encrytedPassword).roles("USER").build();
-
-        mngConfig.withUser(u1);
-        mngConfig.withUser(u2);
-
-        // If Spring BOOT < 2.x (Spring Security 4.x)):
-        // Spring auto add ROLE_
-        // mngConfig.withUser("tom").password("123").roles("USER");
-        // mngConfig.withUser("jerry").password("123").roles("USER");
     }
 }
