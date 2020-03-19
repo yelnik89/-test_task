3427100cae338c31951bd7061f47f8393b8d3de6
yelnik <edv@attrax.digital>
2020-03-19 09:40:20 +0700

    delete tests

--- src/test/java/test_task/demo/DemoApplicationTests.java
+++ src/test/java/test_task/demo/DemoApplicationTests.java
@@ -2,8 +2,12 @@ package test_task.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import test_task.demo.model.Artifact;
+import test_task.demo.service.ArtifactService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,21 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 	}
 
+//	@Autowired
+//	private TestEntityManager entityManager;
+//
+//	@Autowired
+//	private ArtifactService artifactService;
+//
+//	@Test
+//	public void whenFindByName_thenReturnEmployee(){
+//		//given
+//		Artifact art = new Artifact();
+//		artifactService.create(art);
+//		entityManager.persist(art);
+//		entityManager.flush();
+//
+//		//when
+//		Artifact result = artifactService.getArtifactById(art.getId());
+//	}
 }
